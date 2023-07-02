124165923a0dfb7322643d74860ad92df54c17b4
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-07-02 16:51:03 +0100

    style: added scroll feature to hide welcome banner, added shadow to bottom of .top-nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+const Header = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [topStyle, setTopStyle] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    if (currentScrollPos === 0) {
+      setTopStyle(0);
+    } else if (prevScrollPos > currentScrollPos) {
+      setTopStyle(0);
+    } else {
+      setTopStyle(-100); 
+    }
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header>
+      <nav className="top-nav">
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/news">News and Events</a>
+        <a href="/music">Our Music</a>
+      </nav>
+      <h1 className="welcome" style={{ transform: `translateY(${topStyle}%)`, transition: 'transform 0.3s' }}>
+        Welcome to the Neil Coley Big Band
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
+
+
+{/* Original header code
+
 function Header() {
   return (
     <header>
@@ -12,4 +57,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default Header;*/}
