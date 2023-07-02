e6cf8c93a3e809b36fde8ac42e8c973dbd3539bb
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-07-02 12:52:09 +0100

    style: added the code for fade effect in carousel. Can decide which effect you like better

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+// import "swiper/css/effect-fade"; 
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+// import { EffectFade, Autoplay, Pagination, Navigation } from "swiper";
 import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 import { ICarousel } from '@/misc/interfaces';
 
@@ -16,6 +19,7 @@ const Carousel: React.FC<CarouselProps> = ({ carouselPhotos }) => {
   return (
     <Swiper
       spaceBetween={30}
+      // effect={"fade"}
       centeredSlides={true}
       autoplay={{
         delay: 3000,
@@ -27,13 +31,14 @@ const Carousel: React.FC<CarouselProps> = ({ carouselPhotos }) => {
       loop={true}
       speed={500}
       navigation={true}
+      // modules={[EffectFade, Autoplay, Pagination, Navigation]}
       modules={[Autoplay, Pagination, Navigation]}
       className="mySwiper"
     >
       {carouselPhotos.map((photo) => (
         <SwiperSlide key={photo.id}>
           <div className="carousel-photo">
-            <img src={photo.photoURL} alt={photo.name} />
+            <img src={photo.photoURL} alt={photo.name} loading='lazy'/>
           </div>
         </SwiperSlide>
       ))}
